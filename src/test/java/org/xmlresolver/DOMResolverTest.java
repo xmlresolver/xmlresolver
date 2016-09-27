1adbf3f28346f27994684a81ee3015e1333337ff
Christophe Marchand <cmarchand@oxiane.com>
2016-09-27 12:55:43 +0200

    corrected rewriteSystem to use @systemIdStartString instead of @uriStartString

--- src/test/java/org/xmlresolver/DOMResolverTest.java
+++ src/test/java/org/xmlresolver/DOMResolverTest.java
@@ -14,6 +14,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.TestCase;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.w3c.dom.DOMConfiguration;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.ls.DOMImplementationLS;
@@ -38,6 +39,9 @@ public class DOMResolverTest {
         LSParser parser = domLSImpl.createLSParser(DOMImplementationLS.MODE_SYNCHRONOUS, "http://www.w3.org/TR/REC-xml");
         DOMConfiguration config = parser.getDomConfig();
         Catalog c = new Catalog("src/test/resources/catalogs/domresolver.xml");
+        CatalogResult result = c.lookupSystem("foo:ent.xml");
+        assertNotNull("no result found looking up foo:ent.xml", result);
+        assertEquals("rewriteSystem based lookup failed !","http://example.com/entity/ent.xml",result.uri());
         config.setParameter("resource-resolver", new DOMLSResolver(new Resolver(c)));
         parser.parseURI("src/test/resources/documents/dtdtest.xml");
         
