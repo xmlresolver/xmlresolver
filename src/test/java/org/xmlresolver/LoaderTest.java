c4704aaa2955a158e5e525b98c7733f78215832c
Norman Walsh <ndw@nwalsh.com>
2021-06-05 17:54:55 +0100

    Added a test for FileNotFound with the validating loader

--- src/test/java/org/xmlresolver/LoaderTest.java
+++ src/test/java/org/xmlresolver/LoaderTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.xmlresolver.exceptions.CatalogUnavailableException;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.Assert.assertNotNull;
@@ -53,6 +54,21 @@ public class LoaderTest {
             fail();
         } catch (CatalogUnavailableException ex) {
             // pass
+        }
+    }
+
+    @Test
+    public void validatingMissingCatalog() {
+        // File not found isn't a validation error
+        XMLResolverConfiguration config = new XMLResolverConfiguration(Collections.emptyList(), Collections.emptyList());
+        config.setFeature(ResolverFeature.CATALOG_FILES, Arrays.asList("./not-a-catalog-in-sight.xml", "classpath:/catalog.xml"));
+        config.setFeature(ResolverFeature.CATALOG_LOADER_CLASS, "org.xmlresolver.loaders.ValidatingXmlLoader");
+        CatalogManager manager = config.getFeature(ResolverFeature.CATALOG_MANAGER);
+        try {
+            URI rsrc = manager.lookupSystem("https://xmlresolver.com/ns/sample/sample.dtd");
+            assertNotNull(rsrc);
+        } catch (CatalogUnavailableException ex) {
+            fail();
         }
     }
 
