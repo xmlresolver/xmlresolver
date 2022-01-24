4d2bfec0f3b034210f68183667a4946f531cf7f2
Norm Tovey-Walsh <norm@saxonica.com>
2022-01-24 14:52:40 +0000

    Avoid permissions/timing issue on Windows

--- src/test/java/org/xmlresolver/CacheManager.java
+++ src/test/java/org/xmlresolver/CacheManager.java
@@ -7,7 +7,7 @@ import java.io.File;
 import static org.junit.Assert.assertTrue;
 
 public class CacheManager {
-    public File clearCache(String relpath) {
+    public static File clearCache(String relpath) {
         File cache = new File(URIUtils.cwd().getPath() + relpath);
         if (cache.exists()) {
             burnDirectory(cache);
@@ -15,14 +15,15 @@ public class CacheManager {
         return cache;
     }
 
-    protected void burnDirectory(File dir) {
+    protected static void burnDirectory(File dir) {
         File[] contents = dir.listFiles();
         if (contents != null) {
             for (File file: contents) {
                 burnDirectory(file);
             }
         }
-        assertTrue(dir.delete());
+        boolean deleted = dir.delete();
+        assertTrue(deleted);
     }
 
 
