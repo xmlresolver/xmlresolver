ea0a41ab69b6b9cfdeccb5422ef526e18bb3a15e
Norman Walsh <ndw@nwalsh.com>
2024-08-05 13:34:43 +0100

    Make sure the necessary feature is enabled

--- src/test/java/org/xmlresolver/Issue0184Test.java
+++ src/test/java/org/xmlresolver/Issue0184Test.java
@@ -34,6 +34,7 @@ public class Issue0184Test {
     public void parserTest() {
         try {
             ResolvingXMLReader reader = new ResolvingXMLReader(resolver);
+                reader.getResolver().config.setFeature(ResolverFeature.FIX_WINDOWS_SYSTEM_IDENTIFIERS, true);
             String filename = "src/test/iss0184/src/SBBVT0T-Deployment-Flat-mod.xml";
             InputSource source = new InputSource(filename);
             reader.parse(source);
