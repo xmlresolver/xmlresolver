5dd4634ea0f6b8b0228a9ecf3dc60eac5288707d
Jarl Friis <jarl@softace.dk>
2025-08-27 09:12:03 +0200

    Add test templates for SAX adapters

---
+++ src/test/java/org/xmlresolver/adapters/SAX2AdapterTest.java
@@ -0,0 +1,50 @@
+package org.xmlresolver.adapters;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xmlresolver.ResolverFeature;
+import org.xmlresolver.XMLResolver;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+
+class SAX2AdapterTest {
+
+    private XMLReader reader;
+
+    @BeforeEach
+    void setUp() throws ParserConfigurationException, SAXException {
+        XMLResolver resolver = new XMLResolver();
+        ArrayList<String> catalogs = new ArrayList<>();
+        catalogs.add("src/test/resources/saxadapter-catalog.xml");
+        resolver.getConfiguration().setFeature(ResolverFeature.CATALOG_FILES, catalogs);
+
+        reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+        reader.setEntityResolver(resolver.getEntityResolver2());
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getExternalSubset() throws IOException, SAXException {
+        reader.parse(new InputSource("src/test/resources/saxadapter.xml"));
+    }
+
+    @Test
+    void resolveEntity() {
+        // TODO
+    }
+
+    @Test
+    void testResolveEntity() {
+        // TODO
+    }
+}
